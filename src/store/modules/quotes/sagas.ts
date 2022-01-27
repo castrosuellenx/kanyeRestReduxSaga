18616018aba9d07eef15e05f6558901f566805c1
Suellen Castro <castrosuellenx@gmail.com>
2022-01-27 01:29:07 -0300

    update quotes sagas

--- src/store/modules/quotes/sagas.ts
+++ src/store/modules/quotes/sagas.ts
@@ -1,3 +1,24 @@
-import {all} from 'redux-saga/effects';
+import {SagaIterator} from 'redux-saga';
+import {all, call, delay, put, takeLatest} from 'redux-saga/effects';
 
-export default all([]);
+import apiKanye from '../../../services/apiKanye';
+import {failureQuote, setLoading, successQuote} from './actions';
+
+function* getQuote(): SagaIterator {
+  try {
+    yield put(setLoading(true));
+
+    yield delay(500);
+
+    const response = yield call(apiKanye.get, '/');
+    yield put(successQuote(response.data.quote));
+
+    //console.log(response.data.quote);
+  } catch (err) {
+    yield put(failureQuote());
+  } finally {
+    yield put(setLoading(false));
+  }
+}
+
+export default all([takeLatest('@quotes/REQUEST_QUOTE', getQuote)]);
